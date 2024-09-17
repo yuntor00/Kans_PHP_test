6081684f7ff251ea22eb74f6d091fa7a555aec2c
Kans <yuntor00@gmail.com>
2024-09-17 16:31:57 -0500

    test-xor

--- study.local/index.php
+++ study.local/index.php
@@ -1,12 +1,33 @@
 <?php
-echo "Hrllo world";
 
 $var = 'test';
 
 const NAME = 'Kostyassssgit add 123 README.mds';
 define('PAME', 'KOstya123');
 
+$array = [
+    0 => 'test1',
+    1 => 'test2',
+    2 => 'test3'
+];
 
-echo $var;
-echo NAME;
-echo PAME;
+$boolOne = false;
+$boolTwo = true;
+
+if ($boolOne xor $boolTwo) {
+    echo 'hello';
+}
+
+$boolOne = true;
+$boolTwo = false;
+
+if ($boolOne xor $boolTwo) {
+    echo 'hello1';
+}
+
+$boolOne = true;
+$boolTwo = true;
+
+if ($boolOne xor $boolTwo) {
+    echo 'hello2';
+}
